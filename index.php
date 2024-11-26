7b7366c5f4010e96c8bb00fb3842049e95d41a0f
Fas1kD <131455483+Fas1kD@users.noreply.github.com>
2024-11-26 16:09:02 +0100

    upravicka 26.11.2024
    pridani produkt listu, obrazky, nejake styl atd...

--- index.php
+++ index.php
@@ -1,22 +1,71 @@
 <!DOCTYPE html>
 <html lang="cs">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Obchod s nabytkem</title>
 </head>
+
 <body style="background-color: #ECECEC;">
 
+
+<!--Nadpis-->
     <div class="head">
-        <?php include 'templates/header.php';?>
+        <?php include 'templates/header.php'; ?>
     </div>
 
+
+<!--Menu-->
     <div class="menu">
         <div class="menu_neco">
-            <?php include 'templates/menu.php';?>
+            <?php include 'templates/menu.php'; ?>
         </div>
     </div>
 
     <p1 style="text-align: center;">Ahoj světe</p1>
+    <p>ne</p>
+
+
+<!--Produkty-->
+    <div class="product_list"> <!--Usporadani produktu do jednoho "místa"-->
+        <!--samostnatné produkty-->
+        <div class="product">
+            <a href="auto.png" class="clickable-image">
+                <h3>Stůl</h3>
+                <img src="auto.png">
+                <p class="cena">750 kč</p>
+            </a>
+        </div>
+
+        <div class="product">
+            <a href="auto.png" class="clickable-image">
+                <h3>Stůl</h3>
+                <img src="auto.png">
+                <p class="cena">750 kč</p>
+            </a>
+        </div>
+
+        <div class="product">
+            <a href="auto.png" class="clickable-image">
+                <h3>Stůl</h3>
+                <img src="auto.png">
+                <p class="cena">750 kč</p>
+            </a>
+        </div>
+        
+        <div class="product">
+            <a href="auto.png" class="clickable-image">
+                <h3>Stůl</h3>
+                <img src="auto.png">
+                <p class="cena">750 kč</p>
+            </a>
+        </div>
+    </div>
+
+
+
+
 </body>
+
 </html>
